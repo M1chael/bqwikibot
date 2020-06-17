33e273e775f2b479825f5c9ae238853267cf0d6f
M1chael <asoi@mail.ru>
2020-06-17 19:00:44 +0500

    deprecated Kernel#open replaced with URI.open

--- lib/webpage.rb
+++ lib/webpage.rb
@@ -10,7 +10,7 @@ class WebPage
     types = { commentators: 'http://4pda.ru/forum/index.php?act=Stats&view=who&t=<param>', 
     reviews: 'http://4pda.ru/devdb/<param>' }
     @type = options.keys[0]
-    @html = Nokogiri::HTML(open(types[@type].sub('<param>', options.values[0].to_s), 
-      {'User-Agent' => 'BQ wiki bot'}))
+    @html = Nokogiri::HTML(URI.open(types[@type].sub('<param>', options.values[0].to_s), 
+      'User-Agent' => 'BQ wiki bot'))
 	end
 end
